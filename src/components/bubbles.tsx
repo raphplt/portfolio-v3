86dd9c59092937e5b5ae5f65df774c971c24f236
Raphael <plassa_r@etna-alternance.net>
2023-06-04 22:47:46 +0200

    ✨ Update idex page

---
+++ src/components/bubbles.tsx
@@ -0,0 +1,90 @@
+export default function Bubbles() {
+  return (
+    <svg
+      id="visual"
+      //   viewBox="0 0 1900 600"
+      width="100vh"
+      height="600"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+    >
+      <g fill="none" stroke="#ffffff" stroke-width="2">
+        <circle r="12" cx="319" cy="498"></circle>
+        <circle r="-1" cx="876" cy="306"></circle>
+        <circle r="7" cx="686" cy="108"></circle>
+        <circle r="5" cx="627" cy="32"></circle>
+        <circle r="9" cx="516" cy="331"></circle>
+        <circle r="1" cx="232" cy="292"></circle>
+        <circle r="8" cx="171" cy="63"></circle>
+        <circle r="-1" cx="136" cy="519"></circle>
+        <circle r="2" cx="798" cy="376"></circle>
+        <circle r="10" cx="401" cy="33"></circle>
+        <circle r="4" cx="761" cy="164"></circle>
+        <circle r="0" cx="761" cy="550"></circle>
+        <circle r="5" cx="819" cy="279"></circle>
+        <circle r="3" cx="164" cy="466"></circle>
+        <circle r="-2" cx="551" cy="279"></circle>
+        <circle r="-1" cx="783" cy="243"></circle>
+        <circle r="-2" cx="434" cy="294"></circle>
+        <circle r="-2" cx="83" cy="383"></circle>
+        <circle r="0" cx="253" cy="9"></circle>
+        <circle r="-1" cx="308" cy="339"></circle>
+        <circle r="8" cx="9" cy="436"></circle>
+        <circle r="3" cx="428" cy="83"></circle>
+        <circle r="7" cx="421" cy="460"></circle>
+        <circle r="-1" cx="720" cy="308"></circle>
+        <circle r="2" cx="341" cy="348"></circle>
+        <circle r="3" cx="143" cy="241"></circle>
+        <circle r="3" cx="190" cy="6"></circle>
+        <circle r="-1" cx="659" cy="479"></circle>
+        <circle r="4" cx="634" cy="512"></circle>
+        <circle r="3" cx="476" cy="101"></circle>
+        <circle r="-1" cx="445" cy="269"></circle>
+        <circle r="2" cx="29" cy="116"></circle>
+        <circle r="4" cx="694" cy="592"></circle>
+        <circle r="9" cx="115" cy="363"></circle>
+        <circle r="8" cx="300" cy="561"></circle>
+        <circle r="8" cx="847" cy="398"></circle>
+        <circle r="4" cx="849" cy="23"></circle>
+        <circle r="0" cx="355" cy="3"></circle>
+        <circle r="9" cx="705" cy="73"></circle>
+        <circle r="8" cx="586" cy="232"></circle>
+        <circle r="-2" cx="408" cy="205"></circle>
+        <circle r="4" cx="35" cy="466"></circle>
+        <circle r="1" cx="806" cy="554"></circle>
+        <circle r="5" cx="286" cy="79"></circle>
+        <circle r="-1" cx="580" cy="148"></circle>
+        <circle r="3" cx="471" cy="376"></circle>
+        <circle r="0" cx="239" cy="396"></circle>
+        <circle r="8" cx="453" cy="568"></circle>
+        <circle r="6" cx="450" cy="475"></circle>
+        <circle r="7" cx="199" cy="231"></circle>
+        <circle r="2" cx="558" cy="98"></circle>
+        <circle r="-1" cx="833" cy="587"></circle>
+        <circle r="1" cx="23" cy="515"></circle>
+        <circle r="4" cx="235" cy="200"></circle>
+        <circle r="7" cx="635" cy="437"></circle>
+        <circle r="10" cx="142" cy="574"></circle>
+        <circle r="9" cx="741" cy="82"></circle>
+        <circle r="0" cx="131" cy="459"></circle>
+        <circle r="2" cx="167" cy="586"></circle>
+        <circle r="4" cx="80" cy="538"></circle>
+        <circle r="-2" cx="453" cy="53"></circle>
+        <circle r="8" cx="366" cy="298"></circle>
+        <circle r="3" cx="599" cy="57"></circle>
+        <circle r="2" cx="898" cy="289"></circle>
+        <circle r="10" cx="100" cy="497"></circle>
+        <circle r="0" cx="485" cy="505"></circle>
+        <circle r="-2" cx="266" cy="494"></circle>
+        <circle r="5" cx="543" cy="592"></circle>
+        <circle r="2" cx="735" cy="400"></circle>
+        <circle r="4" cx="669" cy="308"></circle>
+        <circle r="4" cx="480" cy="289"></circle>
+        <circle r="0" cx="693" cy="30"></circle>
+        <circle r="6" cx="897" cy="381"></circle>
+        <circle r="-1" cx="240" cy="539"></circle>
+      </g>
+    </svg>
+  );
+}
---
+++ src/components/metadata.tsx
@@ -0,0 +1,7 @@
+export default function Metadata() {
+  return (
+    <div>
+      <h1>Metadata</h1>
+    </div>
+  );
+}
