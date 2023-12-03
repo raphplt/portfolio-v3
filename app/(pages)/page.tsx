d37e1a0cc5212ced2994b530e1aef8d4d7ba8996
plassa_r <plassa_r@etna-alternance.net>
2023-12-03 19:55:23 +0100

    ca marche mdr

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,9 +1,52 @@
-import Sphere from "../_components/scene";
+"use client";
+import { useRef, useState, useEffect } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Box from "@/app/_components/box";
+import * as THREE from "three";
 
-export default function Page() {
+export default function BoxesPage() {
+	const controlsRef = useRef();
+	const [zoomValue, setZoomValue] = useState(0);
+	const [environmentTexture, setEnvironmentTexture] = useState(null);
+
+	const handleZoomChange = () => {
+		const newZoomValue = controlsRef.current?.getDistance();
+		setZoomValue(newZoomValue);
+	};
+
+	useEffect(() => {
+		const textureLoader = new THREE.TextureLoader();
+		textureLoader.load("/assets/space_bg.png", (spaceBgTexture) => {
+			console.log("Texture loaded successfully");
+			setEnvironmentTexture(spaceBgTexture);
+		});
+	}, []);
+
 	return (
-		<div className={`h-[100vh] v-[100vw] bg-space`}>
-			<Sphere />
+		<div className="h-[100vh] v-[100vw] text-white overflow-hidden">
+			<p>Zoom value: {zoomValue}</p>
+			{zoomValue < 20 ? <p>OUAIS CEST GREG</p> : <p>CEST PAS GREG</p>}
+			<Canvas camera={{ position: [0, 0, 35] }}>
+				<ambientLight intensity={2} />
+
+				{/* Utilisez la primitive sphere directement */}
+				<mesh>
+					<sphere args={[50, 60, 60]} />
+					{environmentTexture && (
+						<meshStandardMaterial
+							map={environmentTexture}
+							metalness={1} // Reflective material
+							roughness={0} // Smooth surface
+							side={THREE.BackSide} // Affiche la texture à l'intérieur de la sphère
+						/>
+					)}
+				</mesh>
+
+				<pointLight position={[40, 40, 40]} />
+				<Box position={[0, 0, 0]} />
+				<OrbitControls ref={controlsRef} onChange={handleZoomChange} />
+			</Canvas>
 		</div>
 	);
 }
---
+++ app/_components/box.tsx
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { Sphere as NativeSphere } from "@react-three/drei";
+
+export default function Sphere(props: any) {
+	const mesh: any = useRef();
+
+	useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+	return (
+		<group>
+			{/* Sphere with emissive material */}
+			<NativeSphere args={[1, 32, 32]} {...props} ref={mesh} scale={[6, 6, 6]}>
+				<meshStandardMaterial
+					attach="material"
+					color={"#ff8c00"} // Orange color
+					emissive={"#ff8c00"} // Emissive color for glowing effect
+					emissiveIntensity={0.5} // Adjust the intensity of the glowing effect
+				/>
+			</NativeSphere>
+
+			{/* Ambient light */}
+			<ambientLight intensity={0.5} />
+
+			{/* Directional light for shadows */}
+			<directionalLight position={[10, 10, 5]} intensity={1} castShadow />
+
+			{/* Point lights */}
+			<pointLight position={[-5, 5, 5]} intensity={1} />
+			<pointLight position={[5, -5, -5]} intensity={1} />
+		</group>
+	);
+}
